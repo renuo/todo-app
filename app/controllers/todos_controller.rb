540e32f0a675d6fc32094ade1c37ce4f700bfb58
Simon Isler <simon-isler@gmx.ch>
2019-08-08 11:23:16 +0200

    add database

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,8 +1,21 @@
 class TodosController < ApplicationController
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:todo].inspect
+    @todo = Todo.new(todo_params)
+
+    @todo.save
+    redirect_to @todo
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:title, :description)
   end
 end
+

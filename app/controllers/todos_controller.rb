36fc058e576dffd7e63807b56f899d52e30e8589
Simon Isler <simon-isler@gmx.ch>
2019-08-09 15:04:00 +0200

    add delete checkbox

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -40,6 +40,13 @@ class TodosController < ApplicationController
     end
   end
 
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+
+    redirect_to todos_path
+  end
+
   private
   def todo_params
     params.require(:todo).permit(:title, :description)

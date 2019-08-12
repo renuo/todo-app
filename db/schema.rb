2f1b3ab43e163d1226e3aa720101ea176e76a7a5
Simon Isler <simon-isler@gmx.ch>
2019-08-12 11:35:15 +0200

    add bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_072710) do
+ActiveRecord::Schema.define(version: 2019_08_12_093334) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2019_08_12_072710) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
